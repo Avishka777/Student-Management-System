c7296417cbc8ccb8445f2338c6141da29b7bd551
Avishka777 <a.rathnakumara777@gmail.com>
2024-04-13 20:02:44 +0530

    Modifiy Create Course Page

---
+++ frontend/src/pages/CreateCourses.jsx
@@ -0,0 +1,121 @@
+import React, { useState } from 'react';
+import { TextField, Button, Select, MenuItem, FormControl, InputLabel, Typography } from '@mui/material';
+import { Alert } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
+
+export default function CreateCourses() {
+  const [formData, setFormData] = useState({});
+  const [publishError, setPublishError] = useState(null);
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await fetch('/api/course/create', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (!res.ok) {
+        setPublishError(data.message);
+        return;
+      }
+
+      if (res.ok) {
+        setPublishError(null);
+        navigate(`/dashboard?tab=courses`);
+      }
+    } catch (error) {
+      setPublishError('Something Went Wrong');
+    }
+  };
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+
+  return (
+    <div className='p-3 max-w-3xl mx-auto min-h-screen mt-10'>
+      <div>
+        <Typography variant="h4" align="center" color="primary"> - Create Course - </Typography>
+        <hr className="my-4 border-gray-300 dark:border-gray-600" />
+      </div>
+      <form className='flex flex-col gap-4' onSubmit={handleSubmit}>
+        <div className='mt-2'>
+          <label>Course Name</label>
+          <TextField
+            type='text'
+            placeholder='Course Name'
+            required
+            id='courseName'
+            className='flex-1 mt-2 w-full'
+            onChange={handleChange}
+          />
+        </div>
+        <div className='mt-2'>
+          <label>Course Code</label>
+          <TextField
+            type='text'
+            placeholder='Course Code'
+            required
+            id='courseCode'
+            className='flex-1 mt-2 w-full'
+            onChange={handleChange}
+          />
+        </div>
+        <div className='mt-2'>
+          <label>Course Description</label>
+          <TextField
+            type='text'
+            placeholder='Course Description'
+            required
+            id='courseDescription'
+            className='flex-1 mt-2 w-full'
+            onChange={handleChange}
+          />
+        </div>
+        <div className='mt-2'>
+          <label>Course Price</label>
+          <TextField
+            type='text'
+            placeholder='Course Price'
+            required
+            id='coursePrice'
+            className='flex-1 mt-2 w-full'
+            onChange={handleChange}
+          />
+        </div>
+        <div className='mt-2'>
+          <label>Assign Teacher</label>
+          <FormControl className='flex-1 mt-2 w-full'>
+            <Select
+              value={formData.enrolledTeacher}
+              onChange={handleChange}
+              id='enrolledTeacher'
+            >
+              <MenuItem value=''>
+                <em>Select Lecturer</em>
+              </MenuItem>
+              <MenuItem value='65faafe26a08c9c2d231e3d6'>Mr. Pasan Fernando</MenuItem>
+              <MenuItem value='65faaffa6a08c9c2d231e3d8'>Mr. Dananjaya Abesinghe</MenuItem>
+              <MenuItem value='65fa98425e8a62d5e954e25f'>Mr. Avishka Rathnakumara</MenuItem>
+              <MenuItem value='65fab0136a08c9c2d231e3da'>Mrs. Sudarika Chethani</MenuItem>
+              <MenuItem value='65fab02a6a08c9c2d231e3dc'>Mrs. Dimesha Wijerathne</MenuItem>
+            </Select>
+          </FormControl>
+        </div>
+        <Button type='submit' variant='contained' color='primary' className='mt-2'>
+          Create Course
+        </Button>
+        {publishError && (
+          <Alert className='mt-5' severity='error'>
+            {publishError}
+          </Alert>
+        )}
+      </form>
+    </div>
+  );
+}
