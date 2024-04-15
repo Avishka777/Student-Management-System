208b4c12f659d793dbd5f0456be22ed06150a9b2
Avishka777 <a.rathnakumara777@gmail.com>
2024-04-15 08:00:32 +0530

    Changed Design About Us And Courses Page

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -5,42 +5,51 @@ import obj from '../assets/objective.jpg';
 
 export default function About() {
   return (
-    <Box sx={{ maxWidth: '1200px', margin: 'auto', marginBottom: '40px'  }}>
+    <div>
       {/* Top Section */}
-      <Box sx={{ display: 'flex', flexDirection: 'column', gap: '20px', marginTop: '40px', marginBottom: '40px',  }}>
+      <Box sx={{ display: 'flex', flexDirection: 'column', marginTop: '10rem', marginBottom: '40px', paddingLeft: '10rem', paddingRight: '10rem', margin: 'auto' }}>
       <Box sx={{ display: 'flex', flexDirection: 'row', margin: 'auto' }}>
           <img src={logo} alt="Stark Logo" style={{ height: '100px', width: '100px', borderRadius: '8px'}} />
           <Typography variant="h2" sx={{ color: '#2D3B48', textTransform: 'uppercase', margin: 'auto', fontFamily: 'serif' }}>The Future...</Typography>
         </Box>
         <Divider />
-        <Typography variant="body1" sx={{ color: '#6B7280' }}>
-          The STARK UNIVERSITY was founded in 1999 and is a University Grants Commission (UGC) recognized Institute that stands today as a symbol of excellence in private tertiary education. Welcome to Phoenix Institute, a leading institution dedicated to fostering excellence in education and innovation. At Phoenix, we believe in empowering students with knowledge and skills that prepare them for the challenges of the modern world. With a dynamic curriculum designed to meet the evolving needs of industries and society, we offer a diverse range of programs across various disciplines, including technology, business, arts, and sciences. Our experienced faculty members are committed to providing personalized guidance and support to help students realize their full potential. Through state-of-the-art facilities and interactive learning experiences, we strive to create a vibrant learning community where creativity thrives and ideas flourish. Join us at Phoenix Institute and embark on a journey of discovery, growth, and success.
+        <Typography variant="body1" sx={{ color: '#6B7280' , marginY:'1rem'}}>
+          The PHONENIX INSTITUTE was founded in 1999 and is a University Grants Commission (UGC) recognized Institute that stands today as a symbol of excellence in private 
+          tertiary education. Welcome to Phoenix Institute, a leading institution dedicated to fostering excellence in education and innovation. At Phoenix, we believe in 
+          empowering students with knowledge and skills that prepare them for the challenges of the modern world. With a dynamic curriculum designed to meet the evolving 
+          needs of industries and society, we offer a diverse range of programs across various disciplines, including technology, business, arts, and sciences. Our experienced 
+          faculty members are committed to providing personalized guidance and support to help students realize their full potential. Through state-of-the-art facilities and 
+          interactive learning experiences, we strive to create a vibrant learning community where creativity thrives and ideas flourish. Join us at Phoenix Institute and embark 
+          on a journey of discovery, growth, and success.
         </Typography>
       </Box>
 
       {/* Mission Section */}
-      <Grid container spacing={2} alignItems="center">
-        <Grid item xs={12} md={6}>
-          <Paper elevation={3}>
-            <img src={mission} alt="Our Mission" style={{ width: '100%', borderRadius: '8px' }} />
-            <Box sx={{ padding: '20px' }}>
-              <Typography variant="h5" sx={{ color: '#2D3B48', textTransform: 'uppercase', marginY: '20px' }}>Our Mission...</Typography>
-              <Typography variant="body1" sx={{ color: '#6B7280' }}>To foster academic excellence, inspire innovation, and cultivate lifelong learning within a diverse and inclusive community.</Typography>
-            </Box>
-          </Paper>
-        </Grid>
+      <div style={{ display: 'flex', flexDirection: 'column', marginTop: '1rem', marginBottom: '1rem', paddingLeft: '10rem', paddingRight: '10rem', margin: 'auto' }}>
+        <Grid container spacing={3}>
+          <Grid item xs={12} md={6}>
+            <Paper elevation={3}>
+              <img src={mission} alt="Our Mission" style={{ width: '100%', borderRadius: '8px' }} />
+              <Box sx={{ padding: '20px' }}>
+                <Typography variant="h5" sx={{ color: '#2D3B48', textTransform: 'uppercase', marginY: '20px' }}>Our Mission...</Typography>
+                <Typography variant="body1" sx={{ color: '#6B7280' }}>To foster academic excellence, inspire innovation, and cultivate lifelong learning within a diverse and inclusive community.</Typography>
+              </Box>
+            </Paper>
+          </Grid>
 
-        {/* Objective Section */}
-        <Grid item xs={12} md={6}>
-          <Paper elevation={3}>
-            <img src={obj} alt="Our Objectives" style={{ width: '100%', borderRadius: '8px' }} />
-            <Box sx={{ padding: '20px' }}>
-              <Typography variant="h5" sx={{ color: '#2D3B48', textTransform: 'uppercase', marginY: '20px' }}>Our Objectives...</Typography>
-              <Typography variant="body1" sx={{ color: '#6B7280' }}>To provide quality education, promote research and scholarship, and empower students to thrive in a global society.</Typography>
-            </Box>
-          </Paper>
+          {/* Objective Section */}
+          <Grid item xs={12} md={6}>
+            <Paper elevation={3}>
+              <img src={obj} alt="Our Objectives" style={{ width: '100%', borderRadius: '8px' }} />
+              <Box sx={{ padding: '20px' }}>
+                <Typography variant="h5" sx={{ color: '#2D3B48', textTransform: 'uppercase', marginY: '20px' }}>Our Objectives...</Typography>
+                <Typography variant="body1" sx={{ color: '#6B7280' }}>To provide quality education, promote research and scholarship, and empower students to thrive in a global society.</Typography>
+              </Box>
+            </Paper>
+          </Grid>
         </Grid>
-      </Grid>
-    </Box>
+      </div>
+
+    </div>
   );
 }
--- frontend/src/pages/Courses.jsx
+++ frontend/src/pages/Courses.jsx
@@ -19,18 +19,18 @@ export default function Courses() {
   return (
     <div>
       {/* Top Section */}
-      <Box sx={{ display: 'flex', flexDirection: 'column', gap: '20px', marginTop: '40px', marginBottom: '40px', paddingLeft: '20px', paddingRight: '20px', maxWidth: '1200px', margin: 'auto' }}>
+      <Box sx={{ display: 'flex', flexDirection: 'column', marginTop: '10rem', marginBottom: '40px', paddingLeft: '10rem', paddingRight: '10rem', margin: 'auto' }}>
         <Box sx={{ display: 'flex', flexDirection: 'row', margin: 'auto' }}>
-          <img src={logo} alt="Stark Logo" style={{ height: '100px', width: '100px', borderRadius: '8px'}} />
-          <Typography variant="h2" sx={{ color: '#2D3B48', textTransform: 'uppercase', margin: 'auto', fontFamily: 'serif' }}>OUR COURSES...</Typography>
+          <img src={logo} alt="PHOENIX LOGO" style={{ height: '100px'}} />
+          <Typography variant="h2" sx={{ margin: 'auto', fontFamily: 'serif' }}>OUR COURSES...</Typography>
         </Box>
-        <Divider />
+        <Divider sx={{marginY:'1rem'}} />
         <Typography variant="body1" sx={{ color: '#6B7280' }}>
         We are a leading non-state degree awarding institute approved by the University Grants Commission (UGC) under the Universities Act. We are also members of the 
         Association of Commonwealth Universities (ACU), as well as the International Association of Universities (IAU), and the first Sri Lankan institute to be 
         accredited by the Institution of Engineering & Technology, UK.
         </Typography>
-        <Typography variant="body1" sx={{ color: '#6B7280' }}>
+        <Typography variant="body1" sx={{ color: '#6B7280', marginTop:'1rem' }}>
         We are proud to be listed as a leading and formidable awarding institute authorised and approved by the University Grants Commission (UGC) under the Universities 
         Act, and the International Association of Universities (IAU). Furthermore, not only we are the first Sri Lankan institute to be accredited by the Institute of 
         Engineering & Technology(IET.), UK, our IT degrees are also in turn accredited by the Engineering Council, UK.
@@ -38,10 +38,10 @@ export default function Courses() {
       </Box>
       
       {/* Recent Courses Section */}
-      <Box sx={{ display: 'flex', flexDirection: 'column', paddingLeft: '20px', paddingRight: '20px', maxWidth: '1200px', margin: 'auto' }}>
+      <Box sx={{ display: 'flex', flexDirection: 'column', paddingLeft: '10rem', paddingRight: '10rem', margin: 'auto', marginTop: '2rem' }}>
         {/* Render Recent Courses If Available */}
         {courses && courses.length > 0 && (
-         <Box style={{ display: 'flex', flexDirection: 'column', gap: '20px', marginLeft: 'auto', marginRight: 'auto' ,  marginTop: '2rem'}}>
+         <Box sx={{ display: 'flex', flexDirection: 'column', marginTop: '10rem', marginBottom: '40px', margin: 'auto' }}>
             <h1 style={{ fontSize: '1.8rem', textTransform: 'uppercase', color: '#CB136D', fontWeight: 'bold'}}>ALL Courses...</h1>
             <hr style={{ color: 'gray' }}/>
             <div>
